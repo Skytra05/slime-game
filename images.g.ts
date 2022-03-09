073ad1503a6b9e31d9f88f2ccc9c4ba0a0627674
Skytra05 <101292605+Skytra05@users.noreply.github.com>
2022-03-09 15:56:04 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Yes":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 2 8 8 8 8 8 8 2 8 8 . . 
+. . 8 8 2 2 2 8 8 2 2 2 8 8 . . 
+. . 8 8 8 2 2 2 2 2 2 8 8 8 . . 
+. . 8 8 8 8 8 2 2 8 8 8 8 8 . . 
+. . 8 8 8 8 8 2 2 8 8 8 8 8 . . 
+. . 8 8 8 8 8 2 2 8 8 8 8 8 . . 
+. . 8 8 8 8 8 2 2 8 8 8 8 8 . . 
+. . 8 8 8 8 8 2 2 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "No":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 2 2 8 8 8 8 8 8 2 2 8 . . 
+. . 8 2 2 2 8 8 8 8 8 2 2 8 . . 
+. . 8 2 2 2 2 8 8 8 8 2 2 8 . . 
+. . 8 2 2 2 2 2 8 8 8 2 2 8 . . 
+. . 8 2 2 8 2 2 2 8 8 2 2 8 . . 
+. . 8 2 2 8 8 2 2 2 8 2 2 8 . . 
+. . 8 2 2 8 8 8 2 2 2 2 2 8 . . 
+. . 8 2 2 8 8 8 8 2 2 2 2 8 . . 
+. . 8 2 2 8 8 8 8 8 2 2 2 8 . . 
+. . 8 2 2 8 8 8 8 8 8 2 2 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
